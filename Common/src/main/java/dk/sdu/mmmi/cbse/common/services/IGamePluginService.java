23edb8b660003d21ff09d87417b5dc91e6bc733f
charliewarnes <chillycharlie1@gmail.com>
2024-05-12 16:47:34 +0200

    small changes

--- Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
@@ -10,20 +10,22 @@ import dk.sdu.mmmi.cbse.common.data.World;
 public interface IGamePluginService {
 
     /**
-     * Start is called when the game should start
-     * @param gameData
+     * Start is called when the game should start, and is responsible for instantiating the entities that implements the service into the world
+     * Prepares the game environment for gameplay
+     * @param gameData Information about the game data e.g. game keys
      * @param world
      *
-     * @precondition the game plugin service is not loaded
-     * @postcondition the game plugin service is loaded
+     * @precondition the game plugin service is not loaded. gameData is not null. world is not null
+     * @postcondition the game plugin service is loaded and no error occurs making the game ready for operation
      */
 
     void start(GameData gameData, World world);
 
     /**
-     * Stop is called when the game should stop
-     * @param gameData
-     * @param world
+     * Stop is called when the game should stop, and is responsible for removing the entities that implements the service from the world
+     * Cleans the game environment up after gameplay
+     * @param gameData Information about the game data e.g. game keys
+     * @param world Information about the world e.g. entities in the world
      *
      * @precondition the plugin service is loaded and running
      * @postcondition the plugin service is not running
--- Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
@@ -4,18 +4,18 @@ import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
 /**
- * Is used to process entities after the process is stopped
+ * Is called every game state refresh, IEntityProcessingService will always have been called before
  *
  */
 public interface IPostEntityProcessingService {
 
     /**
      * Is called after the entity process is done
-     * @param gameData
-     * @param world
+     * @param gameData Information about the game data e.g. game keys
+     * @param world Information about the world e.g. entities in the world
      *
      * @precondition the entitiy processing is done
-     * @postcondition the post entity processing is done
+     * @postcondition the post entity processing is done, changes to the current game state refresh have been carried out
      */
 
     void process(GameData gameData, World world);
